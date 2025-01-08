0d748da7a5e12f31d325ffe27fe76ad1a0b3cd9f
rymodkub <rymodkub@gmail.com>
2025-01-08 14:37:22 +0700

    calculator

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import {Header,Content,Footer} from './func-components'
 import Button from './class-components';
-
+import { Calculator2 } from './calculator';
 function App() {
-  return    <Button/>
-    
+  return      <Calculator2/>
 } 
 
 export default App;
--- src/calculator.js
+++ src/calculator.js
@@ -1,23 +1,21 @@
 import React from "react";
-export function Calculator (){
-    n1 = 20
-    n2 = 4
+export function Calculator2 (){
+    const n1 = 20
+    const n2 = 4
 
-    calculate = (op) =>{
+    const calculate = (op) =>{
         let c = `${n1} ${op} ${n2}`
         let r = eval(c)
         alert(`${c} = ${r}`)
     }
-    render(){
         return(
          <div style={{textAlign:'center',marginTop:20}}>
-            <span>{this.n1}</span>&nbsp;
-            <button onClick={() => this.calculate('+')}>+</button>&nbsp;
-            <button onClick={() => this.calculate('-')}>-</button>&nbsp;
-            <button onClick={this.calculate.bind('*')}>*</button>&nbsp;
-            <button onClick={this.calculate.bind('/')}>/</button>&nbsp;
-            <span>{this.n2}</span>
+            <span>{n1}</span>&nbsp;
+            <button onClick={()=>calculate('+')}>+</button>&nbsp;
+            <button onClick={()=>calculate('-')}>-</button>&nbsp;
+            <button onClick={()=>calculate('*')}>*</button>&nbsp;
+            <button onClick={()=>calculate('/')}>/</button>&nbsp;
+            <span>{n2}</span>
          </div>
-        )         
-    }
+        )           
 }
