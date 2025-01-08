6d7bd74866851b81fe5a7dc3fb7caabd427bbcd9
rymodkub <rymodkub@gmail.com>
2025-01-08 15:27:52 +0700

    Event

--- src/App.js
+++ src/App.js
@@ -2,8 +2,9 @@ import React from 'react';
 import {Header,Content,Footer} from './func-components'
 import Button from './class-components';
 import { Calculator2 } from './calculator';
+import { Event } from './event-data';
 function App() {
-  return      <Calculator2/>
+  return      <Event/>
 } 
 
 export default App;
