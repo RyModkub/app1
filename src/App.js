5d2d1bb4e34c4a8e60d67bc8ed7e49b8df6824ef
rymodkub <rymodkub@gmail.com>
2025-01-15 14:12:53 +0700

    add refs-func

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import React from 'react';
+import React,{useRef} from 'react';
 import {Header,Content,Footer} from './func-components'
 import Button from './class-components';
 import { Calculator2 } from './calculator';
 import { Event } from './event-data';
+import RefsFunc from './refs-func';
 function App() {
-  return      <Event/>
+  return      <RefsFunc/>
 } 
 
 export default App;
