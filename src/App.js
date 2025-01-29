49a9733f40b2c3283474358562813f757aa745d6
rymodkub <rymodkub@gmail.com>
2025-01-29 14:15:47 +0700

    add context

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React,{useRef} from 'react';
-import {Header,Content,Footer} from './func-components'
 import Button from './class-components';
 import { Calculator2 } from './calculator';
 import { Event } from './event-data';
 import RefsFunc from './refs-func';
 import Messagebox from './state-func';
+import { userContext } from './context';
+import  Content from './context-content';
 function App() {
-  return      <Messagebox/>
+  return      <userContext.Provider value={'Punyaphat'}>
+              <Content/>
+              </userContext.Provider>
 } 
 
 export default App;
