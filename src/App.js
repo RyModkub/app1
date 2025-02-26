51aafba1ebe9838ae43066038b659c1425ed70b1
rymodkub <rymodkub@gmail.com>
2025-02-26 14:56:22 +0700

    FormSelectFile

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from "react";
 import Routers3 from "./routers3";
 import FormText from "./formtext";
+import FormSelectFile from "./form-select-file";
+
 export default function App(){
-  return <FormText/>
+  return <FormSelectFile/>
 }
